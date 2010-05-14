793062e926836d36b5d2aa28c357283d0b921565
mplatts <plattsmw@gmail.com>
2010-05-14 19:40:57 +1000

    0.5

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,24 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function(){ 
+  var contact = $('#contact');
+  var home = $('#home');
+  var hometext = $('#hometext');
+  var contacttext = $('#contacttext');
+  
+  contacttext.hide();
+  
+  contact.click(function(){
+    home.parent().removeClass('current');
+    contact.parent().addClass('current');
+    hometext.hide();
+    contacttext.show();
+  });
+  
+  home.click(function(){
+    contact.parent().removeClass('current');
+    home.parent().addClass('current');
+    hometext.show();
+    contacttext.hide();
+  });
+});
