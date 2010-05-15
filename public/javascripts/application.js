2cde9433ec83770c98c52f50bb7b8d2bafa2252d
mplatts <plattsmw@gmail.com>
2010-05-16 08:52:04 +1000

    s

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,9 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function(){ 
+  jQuery(function(){
+          $('ul.nav').superfish();
+        });
   var contact = $('#contact');
   var home = $('#home');
   var hometext = $('#hometext');
@@ -21,4 +24,15 @@ $(document).ready(function(){
     hometext.show();
     contacttext.hide();
   });
+  
+  /* ANALYTICS */
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-16411086-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
 });
